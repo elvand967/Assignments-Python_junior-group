dba45d7ccd936be57b36e7a97aece25e48dbc443
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-10-07 10:48:28 +0300

    Практическая работа № 01-1(PyGame) -в работе

--- Assignments-Python_junior-group/Practical01-1(PyGame).py
+++ Assignments-Python_junior-group/Practical01-1(PyGame).py
@@ -7,23 +7,31 @@ import pygame
 import sys
 
 FPS = 60
-W = 700  # ширина экрана
-H = 700  # высота экрана
+W = 500  # ширина экрана
+H = 500  # высота экрана
 WHITE = (255, 255, 255)
 BLUE = (0, 70, 225)
 
 sc = pygame.display.set_mode((W, H))
 clock = pygame.time.Clock()
 
-# координаты и радиус круга
-x = W // 2
-y = H // 2
-r = 25
+# координаты и радиус круга (стартуем с серидины виджета)
+X = x = W // 2 # X - исходные  x - фактические
+Y = y = H // 2
+r = 20
 
-while 1:
+while True:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             sys.exit()
+
+        elif i.type == pygame.KEYUP:
+            if i.key in [pygame.K_LEFT,
+                         pygame.K_RIGHT,
+                         pygame.K_UP,
+                         pygame.K_DOWN]:
+                x = X
+                y = Y
 
     sc.fill(WHITE)
     pygame.draw.circle(sc, BLUE, (x, y), r)
@@ -42,5 +50,5 @@ while 1:
     elif keys[pygame.K_DOWN]:
         y += 3
 
-
-    clock.tick(FPS)
+    # Ограничение количества кадров в секунду, благодаря чему становится
+    clock.tick(FPS) # проще просчитывать анимации и синхронизировать события
